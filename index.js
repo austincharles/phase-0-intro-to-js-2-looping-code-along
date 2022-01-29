1f14e96e71fc6c8c780b1de00f42238a3dfbba05
Austin Holeman <austinholeman@gmail.com>
2022-01-29 14:46:57 -0800

    commit tree 33defd95fb13f2e98d066aae1c0cc53ee55ad117

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const events = ["surprise","surprise", "surprise"];
+
+function writeCards(names, events) {
+    let newArray=[];
+    for ( let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return newArray;
+}
+writeCards(names, events);
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0)
+    console.log(countDown--);
+};
